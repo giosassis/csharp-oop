8cfae4e811e4be0a225c07f76f6024341be1b4c3
Giovana Assis <giovana.sant@hotmai.com>
2023-02-20 20:41:53 -0300

    first commit

---
+++ CheckingAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class CheckingAccount
+    {
+        public int number_branch;
+        public string account;
+        public string owner;
+        public double account_balance;
+
+        public void Deposit(double value)
+        {
+            this.account_balance += value;
+        }
+
+        public void Withdrawal(double value)
+        {
+            if (value <= this.account_balance) { 
+                this.account_balance -= value; 
+            } 
+            else {
+                Console.WriteLine("Error"); 
+            }
+        } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using ByteBank;
+
+CheckingAccount firstPersonAccount = new CheckingAccount();
+firstPersonAccount.owner = "André Silva";
+firstPersonAccount.number_branch = 3306;
+firstPersonAccount.account = "1010-X";
+firstPersonAccount.account_balance = 1050.40;
+
+Console.WriteLine("Balance: " + firstPersonAccount.account_balance);
+
+firstPersonAccount.Deposit(250.87);
+
+Console.WriteLine("Balance: " + firstPersonAccount.account_balance);
+
+firstPersonAccount.Withdrawal(289.90);
+
+Console.WriteLine("Balance: " + firstPersonAccount.account_balance);
